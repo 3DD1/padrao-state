ef393b26300b1ac3df8b56ec875737261d17a303
Edmilson <scovinowork@gmail.com>
2021-10-30 10:16:07 -0300

    classes atleta e interface atletaestado, sets e gets

--- src/main/java/AtletaEstado.java
+++ src/main/java/AtletaEstado.java
@@ -1,3 +1,16 @@
 public interface AtletaEstado {
 
+    String getEstado();
+
+    String descansando(Atleta atleta);
+
+    String caminhando(Atleta atleta);
+
+    String correndo(Atleta atleta);
+
+    String nadando(Atleta atleta);
+
+    String surfando(Atleta atleta);
+
+    String pedalando(Atleta atleta);
 }
